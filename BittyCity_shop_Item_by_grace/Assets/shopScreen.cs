b92ae7936a7377214a3dc1fc0817275cc901e754
Wei Liu <liuw56@mcmaster.ca>
2020-04-07 13:00:06 -0400

    added shop UI

--- BittyCity_shop_Item_by_grace/Assets/shopScreen.cs
+++ BittyCity_shop_Item_by_grace/Assets/shopScreen.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 
 public class ShopScreen : ItemDisplayer {
 
-    
+	public GameObject itemInfoText;
 
 	public void displayItems(Dictionary<Item, int> itemPrices)
 	{
@@ -19,6 +20,16 @@ public class ShopScreen : ItemDisplayer {
     public void acceptItemPurchase()
 	{
 		deselectItem();
+	}
+
+    public void onTrigger()
+	{
+		Debug.log("Hit");
+	}
+
+    public void Start()
+	{
+
 	}
 
 }
--- Shop_Item_Grace_version2/Assets/Scripts/ShopScreen.cs
+++ Shop_Item_Grace_version2/Assets/Scripts/ShopScreen.cs
@@ -2,26 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEditor;
 
 
-public class ShopScreen : ItemDisplayer {
+public class ShopScreen : ItemDisplayer
+{
 
-    public ShopScreen()
-	{
-
-	}
+	public GameObject itemSelected;
 
 	public void displayItems(Dictionary<Item, int> itemPrices)
 	{
-       foreach(Item item in itemPrices.Keys)
+		foreach (Item item in itemPrices.Keys)
 		{
 
 		}
 	}
 
-    public void acceptItemPurchase()
+	public void acceptItemPurchase()
 	{
+
 		deselectItem();
+	}
+
+	
+
+	public void Update()
+	{
+		if (Selection.Contains(itemSelected))
+			Debug.Log("I'm selected!");
 	}
 
 }
