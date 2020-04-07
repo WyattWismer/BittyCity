c6f58a49a06fabfa4394682355d287aa3c22025f
Wyatt Wismer <crazy123frog@gmail.com>
2020-04-07 01:14:50 -0400

    push different versions

---
+++ wyatt_ver_7f1/Assets/Scripts/CitizenControl.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CitizenControl : MonoBehaviour
+{
+    public CityMetricsUI metrics;
+    public StructureControl structureControl;
+    public GameObject citizenPrefab;
+    public float heightOffset;
+
+    // 0 - empty building, 1 - citizen waiting to mate, 2 - two citizens waiting to mate, 3 - mating done, one citizen left
+    private int[,] matingGrid = new int[25, 25];
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void addCitizen(int i, int j)
+    {
+        metrics.setNumCitizens(metrics.getNumCitizens() + 1);
+        metrics.setNumPoints(metrics.getNumPoints() + 500);
+        GameObject new_citizen = Instantiate(citizenPrefab, new Vector3(0.5f + i, heightOffset, 0.5f + j), Quaternion.identity);
+        new_citizen.AddComponent<Citizen>();
+        new_citizen.GetComponent<Citizen>().setup(this, structureControl, new Node(i, j));
+    }
+
+    // returns false if this isn't a valid mating location
+    public bool tryToMate(int i, int j)
+    {
+        if (matingGrid[i,j] <= 1)
+        {
+            matingGrid[i,j]++;
+            return true;
+        }
+        return false;
+    }
+
+    public void giveUpOnMating(int i, int j)
+    {
+        matingGrid[i, j] = 0;
+    }
+
+    public bool hasMated(int i, int j)
+    {
+        if(matingGrid[i,j] >= 2)
+        {
+            if (matingGrid[i,j] == 3)
+            {
+                addCitizen(i, j);
+            }
+            matingGrid[i, j] = (matingGrid[i, j] + 1) % 4; ;
+            return true;
+        }
+        return false;
+    }
+
+    
+}
+
+
---
+++ wyatt_ver_8f1/Assets/Scripts/StructuresInfo.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StructuresInfo : MonoBehaviour
+{
+    public GameObject structurePrefab;
+    public GameObject transparentStructurePrefab;
+    public GameObject deadStructurePrefab;
+    public float heightOffset;
+    private Object[,] grid = new Object[25, 25];
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private Object create(int i, int j, GameObject prefab)
+    {
+        return Instantiate(prefab, new Vector3(0.5f + i, heightOffset, 0.5f + j), Quaternion.identity);
+    }
+
+    public Object createFakeStructure(int i, int j)
+    {
+        return create(i, j, transparentStructurePrefab);
+    }
+
+    public void addDeadStructure(int i, int j)
+    {
+        grid[i, j] = create(i, j, deadStructurePrefab);
+    }
+
+    public void addStructure(int i, int j)
+    {
+        grid[i, j] = create(i, j, structurePrefab);
+    }
+
+    public void removeStructure(int i, int j)
+    {
+        Destroy(grid[i, j]);
+    }
+}
