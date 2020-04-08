037cbdda4b755200933e2317e96fd63b82a02a5d
Nishanth Raveendran <raveendn@mcmaster.ca>
2020-04-07 22:52:16 -0400

    menu stuff

---
+++ wyatt_ver_8f1/Assets/Scripts/MenuActions.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuActions : MonoBehaviour
+{
+    public Slider sliderUI;
+    private Text textSliderValue;
+
+    void Start()
+    {
+        textSliderValue = GetComponent<Text>();
+        ShowSliderValue();
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void ShowSliderValue()
+    {
+        string sliderMessage = sliderUI.value + "m";
+        textSliderValue.text = sliderMessage;
+    }
+
+    public void SceneLoader(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
